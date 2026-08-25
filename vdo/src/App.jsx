666379d89002ecc4fe981860e8f4cd1b14ca0739
MrSachin-05 <sachidanandapanigrahi96@gmail.com>
2026-08-25 09:35:28 +0530

    config of Vdo-player

--- vdo/src/App.jsx
+++ vdo/src/App.jsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import VideoJSPlayer from "./components/videojs-player";
+import ImageKitBasicPlayer from "./components/imagekit-player";
+import BasicVideoPlayer from "./components/video-player";
+
 function App() {
   return (
-    <div>
-      <video src="https://ik.imagekit.io/cezio3tkd/Play-it/play-it_pr.mp4?tr=w-800"
-      width="800" height="450"
-       controls 
-       autoPlay
-       muted
-       loop
-       poster="https://ik.imagekit.io/cezio3tkd/Play-it/play-it_poster.jpg?tr=so-12,fo-auto" 
-      />  
-     
-      <track kind = "subtitles" src="english.vtt" srclang="en" label="English" default />
-       <track kind = "subtitles" src="hindi.vtt" srclang="hi" label="Hindi"/>
+    <div className="App">
+      <VideoJSPlayer />
     </div>
   );
 }
